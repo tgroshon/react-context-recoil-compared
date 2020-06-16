6b8c3b7c3aa2d2693d1c67aad686c17ab116ce4a
Tommy Groshong <tgroshon@users.noreply.github.com>
2020-06-16 19:24:31 -0400

    refactor to external context provider

--- src/1-context-app/context.js
+++ src/1-context-app/context.js
@@ -1,3 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export const SpreadsheetContext = React.createContext([[[]], () => {}]);
+
+export const Provider = ({ size, children }) => {
+  const data = [...Array(size).keys()].map(x =>
+    [...Array(size).keys()].map(x => 0)
+  );
+  const value = React.useState(data);
+
+  return (
+    <SpreadsheetContext.Provider value={value}>
+      {children}
+    </SpreadsheetContext.Provider>
+  );
+};
